709c738dd952d8498ec9c2edd357e173f011ded3
PujaK-7 <137649070+PujaK-7@users.noreply.github.com>
2025-06-26 22:34:07 +0530

    Update turtle_game.py

--- turtle_game.py
+++ turtle_game.py
@@ -5,11 +5,11 @@ window.setup(500,500) # set window dimension
 
 circle_rad=60 # set the radius
 tab=10 #set the tab
-rectangle_height=80 #set the height
+
 
 kat = turtle.Turtle() # create a turtle named kat
 kat.shape("turtle") # kat looks like a turtle
-kat.color('red') # kat has a color
+kat.color('green') # kat has a color
 kat.circle(circle_rad)
 kat.backward(tab)
 kat.circle(circle_rad)
@@ -21,8 +21,3 @@ kat.backward(tab)
 kat.circle(circle_rad)
 #kat.forward(tab*2)
 #kat.right(90)
-#kat.forward(rectangle_height)
-#kat.right(90)
-#kat.forward(rectangle_width)
-#kat.right(90)
-#kat.forward(rectangle_height)
